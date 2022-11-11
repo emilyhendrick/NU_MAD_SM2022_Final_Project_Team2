ebc4a462362bb087b1c1b69649c56a4e42dfcc02
emilyhendrick <emhendrick31@gmail.com>
2022-11-10 19:39:04 -0500

    Frontend add calendar view

--- app/src/main/java/com/example/nu_mad_sm2022_final_project_team2/ui/alarm/AlarmFragment.java
+++ app/src/main/java/com/example/nu_mad_sm2022_final_project_team2/ui/alarm/AlarmFragment.java
@@ -24,7 +24,7 @@ public class AlarmFragment extends Fragment {
         binding = FragmentAlarmBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
+        final TextView textView = binding.alarmPlus;
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
